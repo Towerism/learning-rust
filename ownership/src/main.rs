9862aaa5a6a62c536e609bf5a76532a0cab8cb1b
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-11-07 22:24:38 -0600

    Add reference examples

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    let _s1 = gives_ownership();
+    let s1 = gives_ownership();
+    let s1_len = calculate_length(&s1); // does not take ownership of s1
+    println!("length of {} (s1): {}", s1, s1_len); // s1 can still be used!
     let s2 = String::from("hello");
     let s3 = takes_and_gives_back(s2);
     takes_ownership(s3);
@@ -11,6 +13,7 @@ fn main() {
 fn gives_ownership() -> String {
     let some_string = String::from("hello");
     some_string
+    // &some_string would result in a dangling pointer, and the compiler would complain!
 }
 
 fn takes_and_gives_back(a_string: String) -> String {
@@ -19,4 +22,9 @@ fn takes_and_gives_back(a_string: String) -> String {
 
 fn takes_ownership(some_string: String) {
     println!("{}", some_string);
+} // because we took ownership, some_string is deallocated once it goes out of scope
+
+// calculate length of input string without taking ownership of it
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
