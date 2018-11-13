694bfce4cd3785d67e75c189779fd0c3b78ad596
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-11-13 13:40:29 -0600

    Add adder crate

---
+++ adder/src/lib.rs
@@ -0,0 +1,59 @@
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`", result
+        );
+    }
+
+    #[test]
+    fn wraps_value() {
+        let val = 10;
+        let guess = Guess::new(val);
+        assert_eq!(val, guess.value());
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 and 100, got")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
