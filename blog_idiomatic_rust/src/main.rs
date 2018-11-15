911def981b374adc8e70c6a739c4a302241d4890
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-11-15 15:45:15 -0600

    Implement same blog functionality with idiomatic rust

---
+++ blog_idiomatic_rust/src/main.rs
@@ -0,0 +1,14 @@
+extern crate blog_idiomatic_rust;
+use blog_idiomatic_rust::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
